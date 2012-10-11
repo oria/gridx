6c7c470690aac3d5284a13ce7f8fcb9078755b4b
Xiaowen Zhu <zhuxw1984@gmail.com>
2012-10-11 10:37:47 +0800

    Move stopevent from util to Focus

--- core/util.js
+++ core/util.js
@@ -1,11 +1,4 @@
 define({
-	stopEvent: function(evt){
-		if(evt && evt.stopPropagation && evt.preventDefault){
-			evt.stopPropagation();
-			evt.preventDefault();
-		}
-	},
-
 	biSearch: function(arr, comp){
 		var i = 0, j = arr.length, k;
 		for(k = Math.floor((i + j) / 2); i + 1 < j; k = Math.floor((i + j) / 2)){
