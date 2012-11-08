27a3139affd776f5d0a3a5dd4992ebca8454d937
Xiaowen Zhu <zhuxw1984@gmail.com>
2012-11-08 17:41:03 +0800

    Add scroll bar to TouchScroll

--- modules/TouchScroll.js
+++ modules/TouchScroll.js
@@ -3,9 +3,10 @@ define([
 	"dojo/_base/window",
 	"dojo/_base/event",
 	"dojo/dom",
+	"dojo/dom-construct",
 	"dojo/touch",
 	"../core/_Module"
-], function(declare, win, event, dom, touch, _Module){
+], function(declare, win, event, dom, domConstruct, touch, _Module){
 
 	return declare(_Module, {
 		// summary:
@@ -13,38 +14,75 @@ define([
 
 		name: 'touchScroll',
 
-		forced: ['vScroller', 'hScroller'],
+		required: ['vScroller', 'hScroller'],
 
 		constructor: function(){
-			var g = this.grid,
+			var t = this,
+				g = t.grid,
 				bn = g.bodyNode;
-			this.batchConnect(
+			t.vWrapper = domConstruct.create('div', {
+				'class': 'gridxTouchScrollVWrapper'
+			}, g.mainNode);
+			t.vInner = domConstruct.create('div', {
+				'class': 'gridxTouchScrollVBar'
+			}, t.vWrapper);
+			t.hWrapper = domConstruct.create('div', {
+				'class': 'gridxTouchScrollHWrapper'
+			}, g.mainNode);
+			t.hInner = domConstruct.create('div', {
+				'class': 'gridxTouchScrollHBar'
+			}, t.hWrapper);
+			t.batchConnect(
 				[bn, touch.press, '_start'],
 				[bn, touch.move, '_scroll'],
 				[win.doc, touch.release, '_end']);
 		},
 
 		_start: function(e){
-			this._last = e;
-			dom.setSelectable(this.grid.domNode, false);
+			var t = this,
+				g = t.grid,
+				vns = t.vInner.style,
+				hns = t.hInner.style,
+				bn = g.bodyNode,
+				h = bn.clientHeight,
+				w = bn.clientWidth,
+				sh = g.vScrollerNode.scrollHeight,
+				sw = g.hScrollerNode.scrollWidth;
+			t._last = e;
+			dom.setSelectable(g.domNode, false);
+			t.vWrapper.style.height = (h - 4) + 'px';
+			t.hWrapper.style.width = (w - 4) + 'px';
+			vns.height = h * (h - 4) / sh + 'px';
+			hns.width = w * (w - 4) / sw + 'px';
+			vns.opacity = 1;
+			hns.opacity = 1;
+			t._vr = (h - 4) / sh;
+			t._hr = (w - 4) / sw;
 			event.stop(e);
 		},
 
 		_scroll: function(e){
 			var t = this,
 				g = t.grid,
+				hn = g.hScrollerNode,
+				vn = g.vScrollerNode,
 				last = t._last;
 			if(last){
-				g.hScrollerNode.scrollLeft += last.clientX - e.clientX;
-				g.vScrollerNode.scrollTop += last.clientY - e.clientY;
+				vn.scrollTop += last.clientY - e.clientY;
+				hn.scrollLeft += last.clientX - e.clientX;
+				t.vInner.style.top = vn.scrollTop * t._vr + 'px';
+				t.hInner.style.left = hn.scrollLeft * t._hr + 'px';
 				t._last = e;
 				event.stop(e);
 			}
 		},
 
 		_end: function(e){
-			this._last = null;
-			dom.setSelectable(this.grid.domNode, true);
+			var t = this;
+			t._last = null;
+			dom.setSelectable(t.grid.domNode, true);
+			t.vInner.style.opacity = 0;
+			t.hInner.style.opacity = 0;
 			event.stop(e);
 		}
 	});
