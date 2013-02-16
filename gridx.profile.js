f7b78dfc7603a96893947a278ebb35cc7c3533a8
zhuxw <zhuxw1984@gmail.com>
2013-02-16 12:10:28 +0800

    update gridx.profile.js

--- gridx.profile.js
+++ gridx.profile.js
@@ -1,27 +1,27 @@
 // from dijit/dijit.profile.js
 var profile = (function(){
-    var testResourceRe = /^gridx\/tests\//;
-    var galleryResourceRe = /^gridx\/gallery\//;
-    var buildResourceRe = /^gridx\/build\//;
-    var utilResourceRe = /^gridx\/util\//;
-    
-    var	copyOnly = function(filename, mid){
+	var testResourceRe = /^gridx\/tests\//;
+	var galleryResourceRe = /^gridx\/gallery\//;
+	var buildResourceRe = /^gridx\/build\//;
+	var utilResourceRe = /^gridx\/util\//;
+
+	var	copyOnly = function(filename, mid){
 			var list = {
 				"gridx/gridx.profile":1,
 				"dijit/package.json":1
 			};
 			return (mid in list) 
-                        || (/^gridx\/resources\//.test(mid) && !/\.css$/.test(filename)) 
-                        || /(png|jpg|jpeg|gif|tiff)$/.test(filename);
+						|| (/^gridx\/resources\//.test(mid) && !/\.css$/.test(filename)) 
+						|| /(png|jpg|jpeg|gif|tiff)$/.test(filename);
 		};
 
 	return {
 		resourceTags:{
 			test: function(filename, mid){
 				return testResourceRe.test(mid) 
-                    || galleryResourceRe.test(mid) 
-                    || buildResourceRe.test(mid) 
-                    || utilResourceRe.test(mid);
+					|| galleryResourceRe.test(mid) 
+					|| buildResourceRe.test(mid) 
+					|| utilResourceRe.test(mid);
 			},
 
 			copyOnly: function(filename, mid){
@@ -30,11 +30,11 @@ var profile = (function(){
 
 			amd: function(filename, mid){
 				return !testResourceRe.test(mid) 
-                    && !galleryResourceRe.test(mid) 
-                    && !buildResourceRe.test(mid) 
-                    && !utilResourceRe.test(mid) 
-                    && !copyOnly(filename, mid) 
-                    && /\.js$/.test(filename);
+					&& !galleryResourceRe.test(mid) 
+					&& !buildResourceRe.test(mid) 
+					&& !utilResourceRe.test(mid) 
+					&& !copyOnly(filename, mid) 
+					&& /\.js$/.test(filename);
 			},
 
 			miniExclude: function(filename, mid){
