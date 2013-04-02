e9e3bb996331982ca3b36a4eeccd81f6eb9dc207
zhuxw <zhuxw1984@gmail.com>
2013-04-02 11:20:20 +0800

    Add programmatic single select

--- modules/select/Column.js
+++ modules/select/Column.js
@@ -54,15 +54,15 @@ define([
 		selectById: function(/*String*/id){
 			// summary:
 			//		Select target column by id
+			if(!this.arg('multiple')){
+				this.clear(id);
+			}
 			this._markById(id, 1);
 		},
 		
 		deselectById: function(/*String*/id){
 			// summary:
 			//		Deselect target column by id
-			if(!this.arg('multiple')){
-				this.clear(id);
-			}
 			this._markById(id, 0);
 		},
 		
