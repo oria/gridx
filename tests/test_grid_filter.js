e3d9b935d094aba733709037565c3cffeab25272
Leslie <yurychika@gmail.com>
2013-05-31 17:09:37 +0800

    roll back test grid filter page

--- tests/test_grid_filter.js
+++ tests/test_grid_filter.js
@@ -15,7 +15,6 @@ require([
 		size: 100
 	});
 	
-	jsonStore = new JsonRest({target: 'support/stores/test_grid_filter.php/'});
 
 	layout = [
 		{id: 'id', field: 'id', name: 'Identity', dataType: 'number'},
@@ -36,20 +35,5 @@ require([
 		{id: 'Heard', field: 'Heard', name: 'Heard', dataType: 'boolean'}
 	];
 	
-	layoutServerSide = [
-		{id: 'id', field: 'id', name: 'Identity', dataType: 'number'},
-		{id: 'Genre', field: 'Genre', name: 'Genre', dataType: 'string'},
-		{id: 'Artist', field: 'Artist', name: 'Artist', dataType: 'string'},
-		{id: 'Album', field: 'Album', name: 'Album', dataType: 'string'},
-		{id: 'Name', field: 'Name', name: 'Name', dataType: 'string'},
-		{id: 'Year', field: 'Year', name: 'Year', dataType: 'number'},
-		{id: 'Length', field: 'Length', name: 'Length', dataType: 'string'},
-		{id: 'Track', field: 'Track', name: 'Track', dataType: 'number'},
-		{id: 'Composer', field: 'Composer', name: 'Composer', dataType: 'string'},
-		{id: 'Download Date', field: 'Download Date', name: 'Download Date', dataType: 'date'},
-		{id: 'Last Played', field: 'Last Played', name: 'Last Played', dataType: 'time'},
-		{id: 'Heard', field: 'Heard', name: 'Heard', dataType: 'boolean'}
-	];
-
 	parser.parse();
 });
