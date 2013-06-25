ab240496bf07b4db8d4c1b4d350738f1d56a3fb1
zhuxw <zhuxw1984@gmail.com>
2013-06-25 13:20:08 +0800

    Fix IE10 selecting text during SHIFT range selection

--- modules/extendedSelect/_Base.js
+++ modules/extendedSelect/_Base.js
@@ -66,12 +66,12 @@ define([
 				[doc, 'onmouseup', '_end'],
 				[doc, 'onkeydown', function(e){
 					if(e.keyCode == keys.SHIFT){
-						dom.setSelectable(g.domNode, false);
+						dom.setSelectable(has('ie') > 9 ? doc.body : g.domNode, false);
 					}
 				}],
 				[doc, 'onkeyup', function(e){
 					if(e.keyCode == keys.SHIFT){
-						dom.setSelectable(g.domNode, true);
+						dom.setSelectable(has('ie') > 9 ? doc.body : g.domNode, true);
 					}
 				}]
 			);
