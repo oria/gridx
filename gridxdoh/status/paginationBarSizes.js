ca42c94c8fc2aeeb9af348fcccb6f9d4703bce5d
Leslie <yurychika@gmail.com>
2013-06-26 20:50:40 +0800

    add doh test cases

--- gridxdoh/status/paginationBarSizes.js
+++ gridxdoh/status/paginationBarSizes.js
@@ -19,7 +19,20 @@ define([
 		}
 	},
 	{
-		id: 'paginationBarSizes 1',
+		id: 'paginationBarSizes 2',
+		name: 'If sizes is set and is paginationBarDD, switch buttons have the same count as sizes count',
+		condition: function(grid){
+			return grid.paginationBar && !grid.paginationBar.visibleSteppers && grid.paginationBar.arg('sizes');
+		},
+		checker: function(grid, doh){
+			var selectDijit = dijit.byNode(query('.gridxPagerSizeSwitchTD .dijitSelect ', grid.domNode)[0]);
+			
+			doh.t(grid.paginationBar.arg('sizes').length == selectDijit.getOptions().length, 
+					'switch buttons have the same count as sizes');
+		}
+	},	
+	{
+		id: 'paginationBarSizes 3',
 		name: 'If sizes is set, switch buttons number can be mapped to sizes argument',
 		condition: function(grid){
 			return grid.paginationBar && grid.paginationBar.visibleSteppers && grid.paginationBar.arg('sizes');
@@ -27,11 +40,32 @@ define([
 		checker: function(grid, doh){
 			var sizes = grid.paginationBar.arg('sizes');
 			doh.t(query('.gridxPagerSizeSwitchTD .gridxPagerSizeSwitchBtn', grid.domNode).every(function(node, i){
+				// console.log(i, sizes[i], node.innerHTML);
 				if(sizes[i] === 0){
 					return node.innerHTML === 'All';
 				} 
 				return node.innerHTML == sizes[i];
 			}), 'switch buttons number can be mapped to sizes arugment');
 		}
-	});
+	},
+	{
+		id: 'paginationBarSizes 3',
+		name: 'If sizes is set and is paginationBarDD, switch buttons number can be mapped to sizes argument',
+		condition: function(grid){
+			return grid.paginationBar && !grid.paginationBar.visibleSteppers && grid.paginationBar.arg('sizes');
+		},
+		checker: function(grid, doh){
+			var sizes = grid.paginationBar.arg('sizes');
+			var selectDijit = dijit.byNode(query('.gridxPagerSizeSwitchTD .dijitSelect ', grid.domNode)[0]);
+			
+			doh.t(selectDijit.getOptions().every(function(option, i){
+				// console.log(i, sizes[i], node.innerHTML);
+				if(sizes[i] === 0){
+					return option.label === 'All';
+				} 
+				return parseInt(option.label, 10) === sizes[i];
+			}), 'switch buttons number can be mapped to sizes arugment');
+		}
+	}
+	);
 });
