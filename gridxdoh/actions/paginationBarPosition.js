f7b816812d0190d3ab7ab8a64c0fc689c65be38b
Leslie <yurychika@gmail.com>
2013-06-28 15:43:23 +0800

    add doh cases

---
+++ gridxdoh/actions/paginationBarPosition.js
@@ -0,0 +1,41 @@
+// define([
+	// 'dojo/_base/array',
+	// 'dojo/_base/query',
+	// 'dojo/dom-class',
+	// '../GTest'
+// ], function(array, query, domClass, GTest){
+	// GTest.actionCheckers.push(
+	// {
+		// id: 'paginationBarPosition 1',
+		// name: 'set pagination bar to a new position, call refresh() to take effect',
+		// condition: function(grid){
+			// return grid.paginationBar && grid.paginationBar.arg('position');
+		// },
+		// action: function(grid, doh, done){
+			// grid.paginationBar.position = 'bottom';
+			// grid.body.refresh();
+// 			
+			// doh.t(query('.gridxPaginationBar', grid.footerNode).length, 
+				// 'paginationbar locate in the footer');
+// 
+			// grid.paginationBar.position = 'top';
+			// grid.body.refresh();
+// 			
+			// doh.t(query('.gridxPaginationBar', grid.headerNode).length, 
+				// 'paginationbar locate in the header');
+// 			
+// 				
+			// grid.paginationBar.position = 'both';
+			// grid.body.refresh();
+// 			
+			// doh.t(query('.gridxPaginationBar', grid.footerNode).length, 
+				// 'paginationbar locate in the footer');
+// 
+			// doh.t(query('.gridxPaginationBar', grid.headerNode).length, 
+				// 'paginationbar locate in the header');
+// 			
+			// done.callback();
+		// }
+	// }
+// );
+// });
