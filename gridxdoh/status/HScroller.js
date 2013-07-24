9f8ecbb3b05d3b75b648a6a35e1b9cd43f75a3f7
zhuxw <zhuxw1984@gmail.com>
2013-07-24 11:14:33 +0800

    add test pages

--- gridxdoh/status/HScroller.js
+++ gridxdoh/status/HScroller.js
@@ -26,7 +26,7 @@ define([
 		}
 	},
 	{
-		id: 'core-15',
+		id: 'HScroller 3',
 		name: 'Horizontal scroll bar is as wide as the body',
 		condition: function(grid){
 			return domStyle.get(grid.hScrollerNode, 'display') != 'none';
