eb1b7ec2cb9759396086cd918271ab60eb4ebdab
Leslie <yurychika@gmail.com>
2013-08-22 14:09:23 +0800

    update fix code style

--- modules/ColumnLock.js
+++ modules/ColumnLock.js
@@ -159,18 +159,20 @@ define([
 			}
 			
 			var h1 = dojo.contentBox(r.cells[r.cells.length - 1]).h, 
-				h2 = dojo.marginBox(r.cells[r.cells.length - 1]).h,
-				h3 = domStyle.getComputedStyle(rowNode.firstChild).height;
+				h2 = dojo.marginBox(r.cells[r.cells.length - 1]).h;
 				
-			if(sniff('ie') > 8){	//in IE 9 +, sometimes computed height will contain decimal pixels like 34.4 px, 
-									//plus the height by 1 can force IE to ceil the decimal to integer like from 34.4px to 35px				
+			if(sniff('ie') > 8){	
+				//in IE 9 +, sometimes computed height will contain decimal pixels like 34.4 px, 
+				//so that the locked cells will have different height with the unlocked ones.
+				//plus the height by 1 can force IE to ceil the decimal to integer like from 34.4px to 35px
+				h3 = domStyle.getComputedStyle(rowNode.firstChild).height;
 				if(h3.indexOf('.') >= 0){		//decimal
 					h2++;
 					h1++;
 					
 				}
 			}
-			dojo.style(rowNode.firstChild, 'height', h2 + 'px');
+			domStyle.set(rowNode.firstChild, 'height', h2 + 'px');
 			var lead = isHeader ? this.grid.hLayout.lead : 0,
 				pl = lead,
 				cols = this.grid._columns;
