9754580387e36a9d9e91cd9f901d65499b6c3607
Leslie <yurychika@gmail.com>
2013-08-22 14:01:22 +0800

    update fix comments

--- modules/ColumnLock.js
+++ modules/ColumnLock.js
@@ -157,14 +157,15 @@ define([
 			}
 			
 			var h1 = domGeometry.getContentBox(r.cells[r.cells.length - 1]).h, 
-				h2 = domGeometry.getMarginBox(r.cells[r.cells.length - 1]).h,
-				h3 = domStyle.getComputedStyle(rowNode.firstChild).height;
+				h2 = domGeometry.getMarginBox(r.cells[r.cells.length - 1]).h;
 
-			if(has('ie') > 8){		//in IE 9 +, sometimes computed height will contain decimal pixels like 34.4 px, 
-									//plus the height by 1 can force IE to ceil the decimal to integer like from 34.4px to 35px
+			if(has('ie') > 8){		
+				//in IE 9 +, sometimes computed height will contain decimal pixels like 34.4 px, 
+				//so that the locked cells will have different height with the unlocked ones.
+				//plus the height by 1 can force IE to ceil the decimal to integer like from 34.4px to 35px
 				
-				if(h3.toString().indexOf('.') >= 0){		//decimal
-				// if(Math.ceil(parseInt(h3, 10)) !== parseInt(h3, 10)){		//decimal
+				h3 = domStyle.getComputedStyle(rowNode.firstChild).height;
+				if(String(h3).toString().indexOf('.') >= 0){		//decimal
 					h2++;
 					h1++;
 				}
