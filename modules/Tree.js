1a9776b1f5357b3fca20aa047b967c5f27cd71d1
zhuxw <zhuxw1984@gmail.com>
2013-09-02 17:32:23 +0800

    Fix CTRL+LEFT/RIGHT not work in Chrome

--- modules/Tree.js
+++ modules/Tree.js
@@ -603,7 +603,7 @@ define([
 
 		//Focus------------------------------------------------------------------
 		_initFocus: function(){
-			this.connect(this.grid, 'onCellKeyPress', '_onKey'); 
+			this.connect(this.grid, 'onCellKeyDown', '_onKey'); 
 		},
 
 		_onKey: function(e){
