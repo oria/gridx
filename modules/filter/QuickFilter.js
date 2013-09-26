d10f75151591f2ee32bdf5e2a7f9cf456c6614df
zhuxw <zhuxw1984@gmail.com>
2013-09-26 17:45:15 +0800

    Add Puller to implement mobile quick filter

--- modules/filter/QuickFilter.js
+++ modules/filter/QuickFilter.js
@@ -4,6 +4,7 @@ define([
 	'dojox/gesture/tap',
 	'../../core/_Module',
 	'../../support/QuickFilter',
+	'../Puller',
 	'../Bar'
 ], function(declare, domClass, tap, _Module, QuickFilter){
 
@@ -21,66 +22,27 @@ define([
 	return declare(_Module, {
 		name: 'quickFilter',
 
-		required: ['bar', 'filter'],
+		required: ['bar', 'filter', 'puller'],
 
 		preload: function(){
 			var t = this,
-				bar = t.grid.bar;
-			bar.defs.push({
-				bar: 'top',
-				row: 0,
-				col: 3,
-				pluginClass: QuickFilter,
-				className: 'gridxBarQuickFilter'
-			});
-			if(t.grid.touch){
+				g = t.grid,
+				bar = g.bar,
+				args = {
+					bar: 'top',
+					row: 0,
+					col: 3,
+					pluginClass: QuickFilter,
+					className: 'gridxBarQuickFilter'
+				};
+			if(g.touch){
+				args.bar = 'quickFilter';
+				args.priority = 0.5;
 				bar.loaded.then(function(){
-					bar.topNode.style.display = 'none';
-					bar.topNode.style.height = 0;
-				});
-				t.connect(t.grid.header.domNode, tap.hold, function(){
-					if(t._status > 0){
-						t._slide(0);
-					}else if(!t._status){
-						t._slide(1);
-					}
+					g.puller.bind(bar.quickFilterNode);
 				});
 			}
-		},
-
-		//Private---------------------------------------------------------------------
-		_barHeight: 25,
-
-		_status: 0,
-
-		_slide: function(toShow){
-			var t = this,
-				barNode = t.grid.bar.topNode,
-				barNodeStyle = barNode.style,
-				barHeight = t._barHeight + 'px';
-			if(t._status >= 0){
-				t._status = -1;
-				barNodeStyle.display = '';
-				if(!toShow){
-					//If no height, the animation won't work
-					barNodeStyle.height = barHeight;
-				}
-				domClass.add(barNode, 'gridxBarQuickFilterSlide');
-				setTimeout(function(){
-					barNodeStyle.height = toShow ? barHeight : 0;
-					setTimeout(function(){
-						domClass.remove(barNode, 'gridxBarQuickFilterSlide');
-						if(toShow){
-							barNodeStyle.height = '';
-							t._barHeight = barNode.clientHeight;
-						}else{
-							barNodeStyle.display = 'none';
-						}
-						t.grid.vLayout.reLayout();
-						t._status = toShow ? 1 : 0;
-					}, 500);
-				}, 20);
-			}
+			bar.defs.push(args);
 		}
 	});
 });
