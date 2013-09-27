52d4f939b4fd7bdc4ae3a81c8e8288d4a4b41e9a
Nate Wang <supnate@gmail.com>
2013-09-27 16:19:54 +0800

    Add tree grid test in slanted header test page

--- tests/test_grid_slantedHeader.js
+++ tests/test_grid_slantedHeader.js
@@ -1,5 +1,7 @@
 require([
 	'dojo/parser',
+	'dojo/_base/lang',
+	'dojo/_base/array',
 	'gridx/tests/support/data/MusicData',
 	'gridx/tests/support/stores/Memory',
 	'dojo/store/Memory',
@@ -9,10 +11,11 @@ require([
 	"gridx/modules/SlantedHeader",
 	"gridx/modules/HeaderRegions",
 	"gridx/modules/ExpandableColumn",
+	"gridx/modules/Tree",
 
 	//'gridx/allModules',
 	'dojo/domReady!'
-], function(parser, dataSource, storeFactory, MemoryStore){
+], function(parser, lang, array, dataSource, storeFactory, MemoryStore){
 
 	var datasources = [
 		'Production'
@@ -27,6 +30,7 @@ require([
 		,'Test'
 		,'Development'
 	];
+	datasources.push('Dummy Row','Dummy Row','Dummy Row','Dummy Row','Dummy Row','Dummy Row','Dummy Row','Dummy Row','Dummy Row','Dummy Row','Dummy Row','Dummy Row','Dummy Row' );
 	function randomData(max){
 		max = max || 10;
 		return Math.round(Math.random()*max);
@@ -36,7 +40,7 @@ require([
 	for(var i = 0; i < 20; i++){
 		var item = {
 			id: i + 1
-			,datasource: datasources[i%11]
+			,datasource: datasources[i]
 			//total
 			,critical: randomData(20)
 			,warning: randomData(20)
@@ -77,6 +81,49 @@ require([
 
 	layout = [
 		{field: 'datasource', name: 'Data Sources', width: '130px'},
+
+		{id: 'total', field: 'total', name: 'Total', width: '30px', expanded: true},
+		{field: 'critical', name: 'Critical', width: '30px', _parentColumn: 'total'},
+		{field: 'warning', name: 'Warning', width: '30px', _parentColumn: 'total'},
+
+		{id: 'governance', field: 'governance', name: 'Governace', width: '30px', formatter: formatter},
+		{field: 'monitoring', name: 'Monitoring', width: '30px', _parentColumn: 'governance', formatter: formatter},
+		{field: 'policy', name: 'Policy', width: '30px', _parentColumn: 'governance', formatter: formatter},
+		{field: 'process', name: 'Process', width: '30px', _parentColumn: 'governance', formatter: formatter},
+
+		{id: 'vulnerability', field: 'vulnerability', name: 'Vulnerability', width: '30px', expanded: true, formatter: formatter},
+		{field: 'configuration', name: 'Configuration', width: '30px', _parentColumn: 'vulnerability', formatter: formatter},
+		{field: 'patches', name: 'Patches', width: '30px', _parentColumn: 'vulnerability', formatter: formatter},
+		{field: 'authentication', name: 'Authentication', width: '30px', _parentColumn: 'vulnerability', formatter: formatter},
+		{field: 'privileges', name: 'Privileges', width: '30px', _parentColumn: 'vulnerability', formatter: formatter},
+		{field: 'filesystem', name: 'FileSystem', width: '30px', _parentColumn: 'vulnerability', formatter: formatter},
+
+		{field: 'alerts', name: 'Alerts', width: '30px', formatter: formatter},
+		{field: 'violations', name: 'Violations', width: '30px', formatter: formatter}
+	];
+
+	var treeData = lang.clone(data);
+	var dummyRow = treeData.pop();
+	dummyRow.datasource = 'Dummy Row';
+	var topLevel = array.filter(treeData, function(item){
+		return /Production|Test|Development/.test(item.datasource);
+	});
+	var childrenMap = {
+		Production: array.filter(treeData, function(item){return /Customers|Products|Marketing|Manufacturing/.test(item.datasource);})
+		,Marketing: array.filter(treeData, function(item){return /Sales|Leads/.test(item.datasource);})
+		,Manufacturing: array.filter(treeData, function(item){return /Inventory|Orders/.test(item.datasource);})
+	};
+	treeStore = new MemoryStore({data: topLevel});
+	treeStore.hasChildren = function(id, item){
+		return /Production|Marketing|Manufacturing|Test|Development/.test(item.datasource);
+	};
+
+	treeStore.getChildren = function(item){
+		return childrenMap[item.datasource] || [dummyRow, dummyRow, dummyRow];
+	};
+
+	treeLayout = [
+		{field: 'datasource', name: 'Data Sources', width: '130px', expandLevel: 'all'},
 
 		{id: 'total', field: 'total', name: 'Total', width: '30px', expanded: true},
 		{field: 'critical', name: 'Critical', width: '30px', _parentColumn: 'total'},
