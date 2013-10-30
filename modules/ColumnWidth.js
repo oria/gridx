b57dd56dd64a993347ea05ebc6c1582a5269a64a
Leslie <yurychika@gmail.com>
2013-10-30 17:20:22 +0800

    fix columnWidth issue caused by accuracy losing

--- modules/ColumnWidth.js
+++ modules/ColumnWidth.js
@@ -226,7 +226,7 @@ define([
 					if(c.declaredWidth == 'auto'){
 						autoCols.push(c);
 					}else if(/%$/.test(c.declaredWidth)){
-						var w = parseInt(bodyWidth * parseFloat(c.declaredWidth, 10) / 100 - padBorder, 10);
+						var w = parseFloat(bodyWidth * parseFloat(c.declaredWidth, 10) / 100 - padBorder, 10);
 						//Check if less than zero, prevent error in IE.
 						if(w < 0){
 							w = 0;
@@ -244,7 +244,7 @@ define([
 					if(c.declaredWidth != 'auto'){
 						var headerNode = header.getHeaderNode(c.id),
 							w = !isGroupHeader && needHackPadBorder ? parseFloat(headerNode.style.width, 10) :
-								headerNode.offsetWidth || (domStyle.get(headerNode, 'width') + padBorder);
+								(domStyle.get(headerNode, 'width') + padBorder);
 						if(/%$/.test(c.declaredWidth)){
 							c.width = (w > padBorder ? w - padBorder : 0) + 'px';
 						}
