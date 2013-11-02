bc48a68d12b7f6bb9120cc79270d857b156dfc01
zhuxw <zhuxw1984@gmail.com>
2013-11-02 12:21:01 +0800

    update test case

--- gridxdoh/interntest/sendmail.js
+++ gridxdoh/interntest/sendmail.js
@@ -1,32 +1,36 @@
 var nodemailer = require("nodemailer");
+var url = process.argv[2];
+var content = [
+	'<a href="', url, '">', url, '</a>'
+].join('');
+
 // create reusable transport method (opens pool of SMTP connections)
-var smtpTransport = nodemailer.createTransport("SMTP",{
+var smtpTransport = nodemailer.createTransport("SMTP", {
 	host : "d23m0016.cn.ibm.com",     // smtp server hostname
 	port : "25"                      // smtp server port
 });
 
-var url = process.argv[2];
-
-// setup e-mail data with unicode symbols
-var mailOptions = {
-	from: "gridxadmin@cn.ibm.com", // sender address
-	to: "xwzhu@cn.ibm.com", // list of receivers
-	subject: "Gridx automatic test is finished", // Subject line
-	generateTextFromHTML: true,
-	html: [
-		'<a href="', url, '">', url, '</a>'
-	].join('')
-};
-
-// send mail with defined transport object
-smtpTransport.sendMail(mailOptions, function(error, response){
+function onSent(error, response){
 	if(error){
 		console.log(error);
 	}else{
 		console.log("Message sent: " + response.message);
 	}
+	smtpTransport.close();
+}
 
-	// if you don't want to use this transport object anymore, uncomment following line
-	smtpTransport.close(); // shut down the connection pool, no more messages
+smtpTransport.sendMail({
+	from: "gridxadmin@cn.ibm.com", // sender address
+	to: "xwzhu@cn.ibm.com", // list of receivers
+	subject: "Gridx automatic test is finished", // Subject line
+	generateTextFromHTML: true,
+	html: content
 });
 
+smtpTransport.sendMail({
+	from: "gridxadmin@cn.ibm.com", // sender address
+	to: "zhuxw1984@gmail.com", // list of receivers
+	subject: "Gridx automatic test is finished", // Subject line
+	generateTextFromHTML: true,
+	html: content
+});
