4577f3d5f373c0f9944dbe6cc9899dc27b9098d1
Leslie <yurychika@gmail.com>
2013-11-14 16:19:41 +0800

    fix sort error caused by m.treeMode not exists

--- modules/extendedSelect/_Base.js
+++ modules/extendedSelect/_Base.js
@@ -151,13 +151,13 @@ define([
 					t._currentItem = null;
 					if(extending || t.arg('holdingCtrl')){
 						t._toSelect = !isSelected;
-						if(m.treeMarkMode() && !t._isSelected(item) && t._toSelect){
+						if(g.select.row && m.treeMarkMode() && !t._isSelected(item) && t._toSelect){
 							// toMark = 'mixed';
 							t._toSelect = 'mixed';
 						}
 					}else{
 						t._toSelect = 1;	//1 as true
-						if(m.treeMarkMode() && !t._isSelected(item) && t._toSelect){
+						if(g.select.row && m.treeMarkMode() && !t._isSelected(item) && t._toSelect){
 							// toMark = 'mixed';
 							t._toSelect = 'mixed';
 						}
