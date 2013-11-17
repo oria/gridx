4af8a4bdf856c1a7287fb82e9ec53429cb069be8
zhuxw <zhuxw1984@gmail.com>
2013-11-17 16:08:28 +0800

    update test case

--- interntest/config.js
+++ interntest/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	testPageUrl: "http://localhost/git/dojoprjs/gridxtest/test.html",
+	testPageUrl: "http://localhost/dojospace/gridxdoh/tests/testcases.html",
 	screenshotDir: __dirname + "/../pics",
 	refScreenshotDir: __dirname + "/../refs",
 	testCaseTimeout: 5 * 60 * 1000,
