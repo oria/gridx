aa69ad226f4bc37db17f4e25f9c583edd49828dd
Leslie <yurychika@gmail.com>
2013-12-18 15:05:48 +0800

    add test cases to unselectable row

--- cases/unselectableRows_wd.js
+++ cases/unselectableRows_wd.js
@@ -30,6 +30,48 @@ define([
 						var checked = str.split(' ').indexOf('dijitCheckBoxChecked');
 						assert(checked >= 0, 'select all should not be checked');
 					});
+			},
+			"@select all checkbox should worke correctly after set store or update store[10949]": function(){
+				var selectAllSelector = ".gridxHeader .gridxIndirectSelectionCheckBox ";
+				var checkboxSelector = ".gridxRowHeaderBody .gridxRowHeaderRow[rowid='2']";
+				
+				return this.execute('window.setStore(10);').
+						waitForElementByCss(selectAllSelector, 2000).
+						elementByCss(selectAllSelector).
+						click().
+						wait(200).
+						execute('return grid.select.row.getSelected().length').
+						then(function(len){
+							assert(len > 0, 'select all should work');
+						}).
+						click().
+						wait(200).
+						execute('return grid.select.row.getSelected().length').
+						then(function(len){
+							assert.equal(len, 0, 'disselecte all should work after set store')
+						}).
+						execute('window.restoreStore();').
+						wait(200).
+						end().
+						waitForElementByCss(selectAllSelector, 2000).
+						elementByCss(selectAllSelector).
+						click().
+						wait(200).
+						execute('return grid.select.row.getSelected().length').
+						then(function(len){
+							assert(len > 0, 'select all should work');
+						}).
+						execute('window.removeLastSomeRows()').
+						end().
+						waitForElementByCss(selectAllSelector, 2000).
+						elementByCss(selectAllSelector).
+						click().
+						wait(200).
+						execute('return grid.select.row.getSelected().length').
+						then(function(len){
+							assert.equal(len, 0, 'disselect all should work after update store')
+						})
+
 			}
 		}
 	};
